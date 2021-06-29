c507516520b226b1374611fb8ad873f40ccc712e
Angela Purcell <angelapurcell@Angelas-MBP.attlocal.net>
2021-06-29 11:47:07 -0700

    successfully reading sample file names

--- students/angelapurcell.go
+++ students/angelapurcell.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"log"
 )
 
 
@@ -12,29 +13,40 @@ import (
 
 //list of variables
 var files []string
-var root = "/sample"
+var root = "/Users/angelapurcell/Workspace/personal/gophercizes/renamer/sample"
+
 
 
 //main types for files
-//list of functions
-// func Walk(root string, fn WalkFunc) error {
-// 	fmt.Println("test")
-// }
-
 // IF FOLDER, IGNORE
 // if info.IsDir() {
 //     return nil
 // } 
 
-func getFileNames (files *[]string) filepath.walkFunc{
-	return
-}
-func WalkDir(root string, fn fs.WalkDirFunc) error{
-	fmt.Println("test")
+//list of functions
+func getFileNames (files *[]string) filepath.WalkFunc{
+    return func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            log.Fatal(err)
+        }
+        *files = append(*files, file)
+        return nil
+    }
 }
 //list of methods
 
 //func main
-func main() {
-	fmt.Println("test")
+func main() { 
+	
+	fmt.Println(root)
+	err := filepath.Walk(root, getFileNames(&files))
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files{
+		files = append(files, info.Name)
+		fmt.Println(file)
+	}
+	// files = append(files, info.Name)
+	// err := filepath.Walk(root, visit(&files))
 }
